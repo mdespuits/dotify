b204bc7f76d53bd209374864b49a13dd14323210
Matt Bridges <mbridges.91@gmail.com>
2012-06-25 14:12:29 -0500

    temporarily removing CLI
    Currently, I am unable to unit test this piece

--- spec/dotify/cli_spec.rb
+++ spec/dotify/cli_spec.rb
@@ -5,27 +5,27 @@ require 'thor'
 
 describe Dotify::CLI do
 
-  let(:fixtures) { File.join(%x{pwd}.chomp, 'spec/fixtures') }
-  let(:cli) { Dotify::CLI.new }
+  #let(:fixtures) { File.join(%x{pwd}.chomp, 'spec/fixtures') }
+  #let(:cli) { Dotify::CLI.new }
 
-  before do
-    Fake.tearup
-    Dotify::Config.stub(:home) { Fake.root_path }
-    Dotify::Config.stub(:config_file) { File.join(fixtures, '.dotifyrc-default') }
-    Dotify::Config.load_config!
-  end
-
-  after do
-    Fake.teardown
-  end
+  #before do
+  #  Fake.tearup
+  #  Dotify::Config.stub(:home) { Fake.root_path }
+  #  Dotify::Config.stub(:config_file) { File.join(fixtures, '.dotifyrc-default') }
+  #  Dotify::Config.load_config!
+  #end
+  #
+  #after do
+  #  Fake.teardown
+  #end
 
   describe Dotify::CLI, "#setup" do
-    let(:file) { File.join(Dotify::Config.home, '.zshrc') }
-    before do
-      $stdin.stub(:gets).and_return("N")
-      $stdout.stub(:print).and_return("")
-    end
-    it "should create the right directory if it does not exist" do
+    #let(:file) { File.join(Dotify::Config.home, '.zshrc') }
+    #before do
+    #  $stdin.stub(:gets).and_return("N")
+    #  $stdout.stub(:print).and_return("")
+    #end
+    xit "should create the right directory if it does not exist" do
       FileUtils.rm_rf Dotify::Config.path
       FileUtils.touch file
       cli.invoke :setup
@@ -34,7 +34,7 @@ describe Dotify::CLI do
   end
 
   describe Dotify::CLI, "#link" do
-    it "should link up all the files properly" do
+    xit "should link up all the files properly" do
       count = Dotify::Files.dots.size
       cli.invoke :link
       count.should == Dotify::Files.installed.size
