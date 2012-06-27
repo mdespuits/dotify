16df9430743b62d74cc314f33b47411fa04f0f5d
Matt Bridges <mbridges.91@gmail.com>
2012-06-27 08:49:47 -0500

    adding useful methods to FileList

--- lib/dotify/file_list.rb
+++ lib/dotify/file_list.rb
@@ -1,5 +1,22 @@
 require 'dotify'
+
 module Dotify
   class FileList
+    class << self
+
+      def list(glob)
+        list = filenames(Dir[glob])
+        filter_dot_directories!(list)
+      end
+
+      def filter_dot_directories!(files)
+        files.select { |f| !['.', '..'].include? f }
+      end
+
+      def filenames(files)
+        files.map { |f| Dotify::Files.file_name(f) }
+      end
+
+    end
   end
 end
--- spec/dotify/file_list_spec.rb
+++ spec/dotify/file_list_spec.rb
@@ -4,4 +4,34 @@ require 'dotify/file_list'
 
 describe Dotify::FileList do
 
+  describe Dotify::FileList, "#list" do
+    let(:glob) { '/spec/test/.*' }
+    it "should pull the glob of dotfiles from a directory" do
+      Dir.should_receive(:[]).with(glob).and_return(%w[. .. .vimrc .bashrc .zshrc])
+      Dotify::FileList.list(glob)
+    end
+    it "filter out . and .. directories" do
+      Dir.stub(:[]).with(glob).and_return(%w[. .. .vimrc .bashrc .zshrc])
+      result = Dotify::FileList.list(glob)
+      result.should include '.vimrc'
+      result.should_not include '.'
+      result.should_not include '..'
+    end
+  end
+
+  describe Dotify::FileList, "#filenames" do
+    let(:files) { %w[/spec/test/.vimrc /spec/test/.bashrc /spec/test/.zshrc] }
+    it "should use Files#file_name to change the files" do
+      Dotify::Files.should_receive(:file_name).exactly(files.count).times
+      Dotify::FileList.filenames(files)
+    end
+    it "return only" do
+      result = Dotify::FileList.filenames(files)
+      result.should include '.vimrc'
+      result.should include '.bashrc'
+      result.should include '.zshrc'
+    end
+  end
+
+
 end
