e617908bb201b8115db2e5444c3eccb1b5db59ac
Matt Bridges <mbridges.91@gmail.com>
2012-07-13 08:45:04 -0500

    we do not need to call self in Collection#each

--- lib/dotify/collection.rb
+++ lib/dotify/collection.rb
@@ -13,7 +13,7 @@ module Dotify
 
     # Defined each method for Enumerable
     def each(&block)
-      self.units.each(&block)
+      units.each(&block)
     end
 
     # Linked files are those files which have a
