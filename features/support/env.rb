259e31d24aa44969f6b6dd594e4a28edc3f76102
Matt Bridges <mbridges.91@gmail.com>
2012-07-21 13:22:53 -0500

    increasing aruba wait time for Travis testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,17 +7,23 @@ require 'dotify'
 require 'cucumber/rspec/doubles'
 require 'aruba/cucumber'
 
+Before('@slow_process') do
+
+end
+
 Before do
   @__orig_home = ENV["HOME"]
+  @tmp_home = "/tmp/dotify-test"
+
+  ## Aruba config ##
+  @aruba_timeout_seconds = 6
+  @dirs = [@tmp_home]
+  ENV["PATH"] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 
   # Stub CLI say method
   @cli = Dotify::CLI::Base.new
   @cli.stub(:say)
 
-  ENV["PATH"] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
-
-  @tmp_home = "/tmp/dotify-test"
-  @dirs = [@tmp_home]
   ENV["HOME"] = @tmp_home
   `rm -rf #{File.join(ENV["HOME"], '.bash_profile')}`
   `rm -rf #{File.join(ENV["HOME"], '.gemrc')}`
