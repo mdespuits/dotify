76d9ce4cc48c299c4d558bd6be95b74fd19c2874
Matt Bridges <mbridges.91@gmail.com>
2012-08-15 11:39:48 -0500

    Updating NullObject to add a few helpers
    Thanks to Avdi Grimm and [his article](http://devblog.avdi.org/2011/05/30/null-objects-and-falsiness/) for this idea.

--- lib/dotify/null_object.rb
+++ lib/dotify/null_object.rb
@@ -2,11 +2,26 @@ class NullObject
   def method_missing name, *args, &blk
     self
   end
-  def nil?;     true;   end
-  def present?; !nil?;  end
+
+  def tap;      self;   end
   def to_a;     [];     end
   def to_ary;   [];     end
   def to_s;     '';     end
   def to_i;     0;      end
   def to_f;     0.0;    end
+
+  def to_value
+    nil # for some conditional values
+  end
+
+  def nil?
+    true
+  end
+  alias :blank? :nil?
+  alias :empty? :nil?
+
+  def present?
+    false
+  end
+  alias :any? :present?
 end
