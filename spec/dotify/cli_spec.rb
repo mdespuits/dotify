bc6dc7dd9c3cadc62e88986ee38c7f820f1f02dd
Matt Bridges <mbridges.91@gmail.com>
2012-08-15 09:39:48 -0500

    testing for existence of important cli tasks

--- spec/dotify/cli_spec.rb
+++ spec/dotify/cli_spec.rb
@@ -4,5 +4,15 @@ require 'thor'
 
 module Dotify
   describe CLI::Base do
+    describe "essential tasks existence tests" do
+      it { should respond_to :install }
+      it { should respond_to :setup } # remove and merge with install?
+      it { should respond_to :link }
+      it { should respond_to :unlink }
+      it { should respond_to :github }
+      it { should respond_to :repo }
+      it { should respond_to :edit }
+      it { should respond_to :version }
+    end
   end
 end
