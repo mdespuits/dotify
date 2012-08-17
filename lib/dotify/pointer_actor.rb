858723443199aa4ea8fa6c7cf2585ada2fc84ca8
Matt Bridges <mbridges.91@gmail.com>
2012-08-16 22:44:02 -0500

    unit tests for PointerActor
    Still not a fan of the classname, but it works
    for now...

--- lib/dotify/pointer_actor.rb
+++ lib/dotify/pointer_actor.rb
@@ -12,26 +12,44 @@ module Dotify
       @destination = pointer.destination
     end
 
+    # Linking from source links the destination file
+    # to the source file (the file stored within Dotify)
+    # given the source file is in existence and the destination
+    # is or is being overwritten.
     def link_from_source
+      touch source
+      remove_destination
+      link!
     end
     alias :link_to_destination :link_from_source
 
     def link_to_source
+      remove_source
+      move_to_source
+      link!
     end
     alias :link_from_destination :link_to_source
 
     def move_to_source
+      touch destination
+      move destination, source
     end
 
     def move_to_destination
+      touch source
+      move source, destination
     end
 
     def remove_source
-      rm_rf source
+      rm_rf source, :secure => true
     end
 
     def remove_destination
-      rm_rf destination
+      rm_rf destination, :secure => true
+    end
+
+    def link!
+      ln_sf source, destination
     end
 
   end
--- spec/dotify/pointer_actor_spec.rb
+++ spec/dotify/pointer_actor_spec.rb
@@ -41,5 +41,58 @@ module Dotify
       it { should respond_to :remove_destination }
     end
 
+    describe "#remove_source" do
+      before do
+        subject.should_receive(:rm_rf).with(subject.source, :secure => true).once
+      end
+      it { subject.remove_source }
+    end
+
+    describe "#remove_destination" do
+      before do
+        subject.should_receive(:rm_rf).with(subject.destination, :secure => true).once
+      end
+      it { subject.remove_destination }
+    end
+
+    describe "#link_from_source" do
+      before do
+        subject.should_receive(:remove_destination).once
+        subject.should_receive(:link!).once
+      end
+      it { subject.link_from_source }
+    end
+
+    describe "#link_to_source" do
+      before do
+        subject.should_receive(:remove_source).once
+        subject.should_receive(:move_to_source)
+        subject.should_receive(:link!).once
+      end
+      it { subject.link_from_destination }
+    end
+
+    describe "#move_to_source" do
+      before do
+        subject.should_receive(:touch).with(subject.destination).once
+        subject.should_receive(:move).with(subject.destination, subject.source)
+      end
+      it { subject.move_to_source }
+    end
+
+    describe "#move_to_destination" do
+      before do
+        subject.should_receive(:touch).with(subject.source).once
+        subject.should_receive(:move).with(subject.source, subject.destination)
+      end
+      it { subject.move_to_destination }
+    end
+
+    describe "#link!" do
+      before do
+        subject.should_receive(:ln_sf).with(subject.source, subject.destination)
+      end
+      it { subject.link! }
+    end
   end
 end
