993e92d62e8dccfcd4eb0ccc0112642d7c0a5792
Matt Bridges <mbridges.91@gmail.com>
2012-08-16 22:20:05 -0500

    reorganizing and updating action features

--- features/step_definitions/actions.rb
+++ features/step_definitions/actions.rb
@@ -4,10 +4,14 @@ Given /^a pointer within an actor with attributes\:$/ do |table|
   @actor = Dotify::PointerActor.new(@pointer)
 end
 
-Given /^I call "(.*)" on the actor/ do |action|
+When /^I call "(.*)" on the actor/ do |action|
   @actor.send(action)
 end
 
 Then /^the (source|destination) should not exist/ do |what|
   File.exists?(@actor.send(what || "destination")).should be_false
+end
+
+Then /^the (source|destination) should exist/ do |what|
+  File.exists?(@actor.send(what || "destination")).should be_true
 end
