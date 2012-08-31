d4420bab489f3354a1317ff823c9d98fcf46db5c
Matt Bridges <mbridges.91@gmail.com>
2012-08-31 09:11:03 -0500

    using class << self pattern in FileList class

--- lib/dotify/file_list.rb
+++ lib/dotify/file_list.rb
@@ -1,33 +1,35 @@
 module Dotify
   class FileList
+    class << self
 
-    def self.pointers
-      @pointers ||= []
-    end
-
-    # Add the list of Pointers from the dotfiles every time.
-    # Whether they are linked or not initially makes no
-    # difference.
-    def self.dotfile_pointers
-      Dir["#{Path.home}/.*"].each do |file|
-        self.add Pointer.new(Path.dotify_path(File.basename(file)), file)
+      def pointers
+        @pointers ||= []
       end
-    end
 
-    def self.add(*new_pointers)
-      new_pointers.each do |p|
-        # Only add the new pointer if the source has not already been taken to avoid conflicts
-        pointers << p unless sources.include? p.source
+      # Add the list of Pointers from the dotfiles every time.
+      # Whether they are linked or not initially makes no
+      # difference.
+      def dotfile_pointers
+        Dir["#{Path.home}/.*"].each do |file|
+          self.add Pointer.new(Path.dotify_path(File.basename(file)), file)
+        end
       end
-    end
 
-    def self.destinations
-      pointers.map(&:destination)
-    end
+      def add(*new_pointers)
+        new_pointers.each do |p|
+          # Only add the new pointer if the source has not already been taken to avoid conflicts
+          pointers << p unless sources.include? p.source
+        end
+      end
 
-    def self.sources
-      pointers.map(&:source)
-    end
+      def destinations
+        pointers.map(&:destination)
+      end
 
+      def sources
+        pointers.map(&:source)
+      end
+
+    end
   end
 end
