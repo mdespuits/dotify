8fc55f67ee1a598649815c079d111961a27cc83d
Matt Bridges <mbridges.91@gmail.com>
2013-03-04 15:11:13 -0600

    pointers are equal as long as their sources are the same

--- lib/dotify/pointer.rb
+++ lib/dotify/pointer.rb
@@ -1,5 +1,10 @@
 module Dotify
   class Pointer < Struct.new(:source, :destination)
+
+    def eql?(other)
+      self.source == other.source
+    end
+
     def to_s
       %{#<Dotify::Pointer source: "#{source}" destination: "#{destination}">}
     end
--- spec/dotify/pointer_spec.rb
+++ spec/dotify/pointer_spec.rb
@@ -10,5 +10,21 @@ module Dotify
       its(:source) { should == source }
       its(:destination) { should == destination }
     end
+
+    describe "#eql?" do
+      subject(:pointer) { Pointer.new("example", "destination") }
+      context "when they are equal" do
+        let(:other) { Pointer.new("example", "destination") }
+        it { pointer.eql?(other).should be_true }
+      end
+      context "when they have same source but different destination" do
+        let(:other) { Pointer.new("example", "other-destination")}
+        it { pointer.eql?(other).should be_true }
+      end
+      context "when they are not equal" do
+        let(:other) { Pointer.new("examples", "destination") }
+        it { pointer.eql?(other).should be_false }
+      end
+    end
   end
 end
