194ec075f119dd7c1f5f6daa879528cffa656037
Matt Bridges <mbridges.91@gmail.com>
2013-03-04 18:07:32 -0600

    making instance a null object as we do not care about anything else

--- spec/dotify_spec.rb
+++ spec/dotify_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 
 describe Dotify do
   subject { described_class }
+  let(:instance) { double('instance').as_null_object }
 
   describe ".in_instance" do
-    let(:inst) { double('instance') }
     it "should temporarily set the instance to the given argument and execute the block" do
-      Dotify.in_instance(inst) { 0 }.should == 0
+      Dotify.in_instance(instance) { 0 }.should == 0
       Dotify.instance_eval { @instance }.should be_nil
     end
   end
 
   describe ".setup" do
     it 'should run in the context of the Maid::Maid instance' do
-      instance = double('instance')
       instance.should_receive(:foo)
       Dotify.in_instance(instance) do
         Dotify.setup { foo }
