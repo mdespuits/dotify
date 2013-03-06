fdef8be690c3226b8ed503ba14f103f045b18789
Matt Bridges <mbridges.91@gmail.com>
2013-03-06 10:57:33 -0600

    wrap the Configure specs in the Dotify module to work on 1.9.2

--- spec/dotify/configure_spec.rb
+++ spec/dotify/configure_spec.rb
@@ -1,108 +1,109 @@
 require 'spec_helper'
 
-include Dotify
+module Dotify
 
-class Configure
-  def self.reset!
-    @links = []
+  class Configure
+    def self.reset!
+      @links = []
+    end
   end
-end
 
-describe Configure do
-  describe "#start" do
+  describe Configure do
+    describe "#start" do
 
-    before { Configure.reset! }
+      before { Configure.reset! }
 
-    context "without any settings" do
-      subject do
-        Configure.start { |d| }
-      end
-      its(:editor) { should == 'vim' }
-    end
-
-    context "with and editor set" do
-      subject do
-        Configure.start do |d|
-          d.editor { "emacs" }
+      context "without any settings" do
+        subject do
+          Configure.start { |d| }
         end
+        its(:editor) { should == 'vim' }
       end
-      its(:editor) { should == 'emacs' }
-    end
 
-    context "with a particular platform set" do
-      subject do
-        Configure.start do |d|
-          d.platform :osx do |d|
+      context "with and editor set" do
+        subject do
+          Configure.start do |d|
             d.editor { "emacs" }
           end
         end
-      end
-      context "when on OSX" do
-        before { OperatingSystem.stub(:guess) { 'osx' }}
         its(:editor) { should == 'emacs' }
       end
-      context "when on OSX" do
-        before { OperatingSystem.stub(:guess) { 'linux' }}
-        its(:editor) { should == 'vim' }
-      end
-      context "when no platform is defined by accident" do
-        let(:config) do
+
+      context "with a particular platform set" do
+        subject do
           Configure.start do |d|
-            d.platform do |d|
+            d.platform :osx do |d|
               d.editor { "emacs" }
             end
           end
         end
-        it { expect { config }.to raise_error(ArgumentError) }
-      end
-    end
-
-    context "with an editor name set as a symbol" do
-      subject do
-        Configure.start do |d|
-          d.editor { :emacs }
+        context "when on OSX" do
+          before { OperatingSystem.stub(:guess) { 'osx' }}
+          its(:editor) { should == 'emacs' }
+        end
+        context "when on OSX" do
+          before { OperatingSystem.stub(:guess) { 'linux' }}
+          its(:editor) { should == 'vim' }
+        end
+        context "when no platform is defined by accident" do
+          let(:config) do
+            Configure.start do |d|
+              d.platform do |d|
+                d.editor { "emacs" }
+              end
+            end
+          end
+          it { expect { config }.to raise_error(ArgumentError) }
         end
       end
-      its(:editor) { should == 'emacs' }
-    end
 
-    context "calling ignore multiple times with same value" do
-      subject do
-        Configure.start do |d|
-          d.ignore { '.example' }
-          d.ignore { '.example' }
-          d.ignore { '.example' }
+      context "with an editor name set as a symbol" do
+        subject do
+          Configure.start do |d|
+            d.editor { :emacs }
+          end
         end
+        its(:editor) { should == 'emacs' }
       end
-      its(:ignore) { should have(3).items }
-      its(:ignore) { should include '.example' }
-    end
 
-    context "with link should create a pointer" do
-      let(:config) do
-        Configure.start do |d|
-          d.symlink("source", "destination")
-          d.symlink("another-source", "another-destination")
+      context "calling ignore multiple times with same value" do
+        subject do
+          Configure.start do |d|
+            d.ignore { '.example' }
+            d.ignore { '.example' }
+            d.ignore { '.example' }
+          end
         end
+        its(:ignore) { should have(3).items }
+        its(:ignore) { should include '.example' }
       end
-      subject { config.symlinks }
-      it { should have(2).items }
-      it { subject.first.source.should =~ /source\z/i }
-      it { subject.first.destination.should =~ /destination\z/i }
-    end
 
-    context "with ignore settings with defaults" do
-      subject do
-        Configure.start do |d|
-          d.ignore { '.sublime' }
+      context "with link should create a pointer" do
+        let(:config) do
+          Configure.start do |d|
+            d.symlink("source", "destination")
+            d.symlink("another-source", "another-destination")
+          end
         end
+        subject { config.symlinks }
+        it { should have(2).items }
+        it { subject.first.source.should =~ /source\z/i }
+        it { subject.first.destination.should =~ /destination\z/i }
       end
-      its(:editor) { should == 'vim' }
-      its(:ignore) { should have(3).items }
-      its(:ignore) { should include '.DS_Store' }
-      its(:ignore) { should include '.git' }
-      its(:ignore) { should include '.sublime' }
-    end
 
+      context "with ignore settings with defaults" do
+        subject do
+          Configure.start do |d|
+            d.ignore { '.sublime' }
+          end
+        end
+        its(:editor) { should == 'vim' }
+        its(:ignore) { should have(3).items }
+        its(:ignore) { should include '.DS_Store' }
+        its(:ignore) { should include '.git' }
+        its(:ignore) { should include '.sublime' }
+      end
+
+    end
   end
 end
